d47c01ac9211efbb73083922b1defd63f0f4b080
Zamua Nasrawt <zamua2012@gmail.com>
2017-04-27 18:49:40 -0400

    Change flood fill to toggle between origin cells. Still not working.

--- src/ClusterSeeder.java
+++ src/ClusterSeeder.java
@@ -34,39 +34,57 @@ public class ClusterSeeder
 
     public void seed()
     {
-        tandemFloodFill(gridA, a1Row, a1Col, a2Row, a2Col);
-        tandemFloodFill(gridB, b1Row, b1Col, b2Row, b2Col);
+        tandemFloodFill(gridA, a1Row, a1Col, a2Row, a2Col, true);
+        // tandemFloodFill(gridB, b1Row, b1Col, b2Row, b2Col, true);
     }
 
-    private void tandemFloodFill(int[][] grid, int row1, int col1, int row2, int col2)
+    private void tandemFloodFill(int[][] grid, int row1, int col1, int row2, int col2, boolean toggle)
     {
-        if (inBounds(grid, row1, col1) && grid[row1][col1] != 0 &&
-            inBounds(grid, row2, col2) && grid[row2][col2] != 0) {
-            return;
-        }
+        if (toggle) { // seed 1
+            System.err.printf("***1***\trow: %d\tcol:%d\n", row1, col1);
+            if (!inBounds(grid, row1, col1)) {
+                //System.err.printf("row1: %d\tcol2: %d\n", row1, col1);
+                return;
+            }
+            if (grid[row1][col1] != 0) {
+                return;
+            }
 
-        if (inBounds(grid, row1, col1) && grid[row1][col1] == 0) {
             grid[row1][col1] = 1;
-        }
+            //System.err.println("Filled 1");
+        } else { // seed 2
+            System.err.printf("***2***\trow: %d\tcol:%d\n", row2, col2);
+            if (!inBounds(grid, row2, col2)) {
+                return;
+            }
+            if (grid[row2][col2] != 0) {
+                return;
+            }
 
-        if (inBounds(grid, row2, col2) && grid[row2][col2] == 0) {
             grid[row2][col2] = 2;
         }
 
         for (int i = 0; i < 4; i++) {
+            System.err.printf("r: %d\tc: %d\n", row[i], col[i]);
             int ri1 = row1 + row[i];
             int ri2 = row2 + row[i];
             int ci1 = col1 + col[i];
             int ci2 = col2 + col[i];
-
-            System.err.printf("ri1: %d\tri2: %d\tci1: %d\tci2: %d\n", ri1, ri2, ci1, ci2);
-            tandemFloodFill(grid, ri1, ci1, ri2, ci2);
+            if (toggle) {
+                //System.err.printf("ri1: %d\tci1: %d\n", ri1, ci1);
+                // just filled from seed 1, so let seed 2 proceed with old
+                // values
+                tandemFloodFill(grid, ri1, ci1, row2, col2, !toggle);
+            } else {
+                // just filled from seed 2, so let seed 1 proceed with old
+                // values
+                tandemFloodFill(grid, row1, col1, ri2, ci2, !toggle);
+            }
         }
     }
 
     private boolean inBounds(int[][] grid, int row, int col)
     {
-        System.err.println(row > 0 && col > 0 && row < grid.length && col < grid[row].length);
-        return row > 0 && col > 0 && row < grid.length && col < grid[row].length;
+        return row >= 0 && col >= 0 && row < grid.length && col < grid[row].length;
     }
 }
