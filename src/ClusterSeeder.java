1a10c6ee0812eaf6cd981964c931bee8f9454746
Zamua Nasrawt <zamua2012@gmail.com>
2017-04-30 23:22:12 -0400

    Fix floodfill (not limited to 2 conneted components)

--- src/ClusterSeeder.java
+++ src/ClusterSeeder.java
@@ -1,32 +1,33 @@
+import java.awt.Point;
+import java.util.HashSet;
 import java.util.Random;
 
 public class ClusterSeeder
 {
     private Random rowRand;
     private Random colRand;
-    private int a1Row, a1Col;
-    private int a2Row, a2Col;
-    private int b1Row, b1Col;
-    private int b2Row, b2Col;
     private int[][] gridA, gridB;
+    private Point a1Seed;
+    private Point a2Seed;
+    private Point b1Seed;
+    private Point b2Seed;
 
-    private final int[] row = { -1, 0, 1, 0 };
-    private final int[] col = { 0, -1, 0, 1 };
+    private final int[] row = { 1, 0, -1, 0 };
+    private final int[] col = { 0, 1, 0, -1 };
+
+    private HashSet<Point> seeds;
 
     public ClusterSeeder(int[][] gridA, int[][] gridB)
     {
         this.rowRand = new Random();
         this.colRand = new Random();
 
-        this.a1Row = rowRand.nextInt(gridA.length);
-        this.a1Col = colRand.nextInt(gridA[0].length);
-        this.a2Row = rowRand.nextInt(gridA.length);
-        this.a2Col = colRand.nextInt(gridA[0].length);
+        this.seeds = new HashSet<Point>();
 
-        this.b1Row = rowRand.nextInt(gridA.length);
-        this.b1Col = colRand.nextInt(gridA[0].length);
-        this.b2Row = rowRand.nextInt(gridA.length);
-        this.b2Col = colRand.nextInt(gridA[0].length);
+        this.a1Seed = generateUniqueSeed(gridA);
+        this.a2Seed = generateUniqueSeed(gridA);
+        this.b1Seed = generateUniqueSeed(gridB);
+        this.b2Seed = generateUniqueSeed(gridB);
 
         this.gridA = gridA;
         this.gridB = gridB;
@@ -34,51 +35,43 @@ public class ClusterSeeder
 
     public void seed()
     {
-        tandemFloodFill(gridA, a1Row, a1Col, a2Row, a2Col, true);
+        System.err.printf("A: (%d,%d)\t B: (%d,%d)\n", a1Seed.y, a1Seed.x, a2Seed.y, a2Seed.x);
+        tandemFloodFill(gridA, a1Seed.y, a1Seed.x, a2Seed.y, a2Seed.x, true);
         // tandemFloodFill(gridB, b1Row, b1Col, b2Row, b2Col, true);
+    }
+
+    private Point generateUniqueSeed(int[][] grid)
+    {
+        Point seed = new Point(colRand.nextInt(grid[0].length), rowRand.nextInt(grid.length));
+
+        while (seeds.contains(seed)) {
+            seed = new Point(colRand.nextInt(grid[0].length), rowRand.nextInt(grid.length));
+        }
+
+        return seed;
     }
 
     private void tandemFloodFill(int[][] grid, int row1, int col1, int row2, int col2, boolean toggle)
     {
         if (toggle) { // seed 1
-            System.err.printf("***1***\trow: %d\tcol:%d\n", row1, col1);
-            if (!inBounds(grid, row1, col1)) {
-                //System.err.printf("row1: %d\tcol2: %d\n", row1, col1);
-                return;
-            }
-            if (grid[row1][col1] != 0) {
-                return;
-            }
-
             grid[row1][col1] = 1;
-            //System.err.println("Filled 1");
         } else { // seed 2
-            System.err.printf("***2***\trow: %d\tcol:%d\n", row2, col2);
-            if (!inBounds(grid, row2, col2)) {
-                return;
-            }
-            if (grid[row2][col2] != 0) {
-                return;
-            }
-
             grid[row2][col2] = 2;
         }
 
         for (int i = 0; i < 4; i++) {
-            System.err.printf("r: %d\tc: %d\n", row[i], col[i]);
             int ri1 = row1 + row[i];
-            int ri2 = row2 + row[i];
             int ci1 = col1 + col[i];
+            int ri2 = row2 + row[i];
             int ci2 = col2 + col[i];
             if (toggle) {
-                //System.err.printf("ri1: %d\tci1: %d\n", ri1, ci1);
-                // just filled from seed 1, so let seed 2 proceed with old
-                // values
-                tandemFloodFill(grid, ri1, ci1, row2, col2, !toggle);
+                if (inBounds(grid, ri1, ci1) && grid[ri1][ci1] == 0) {
+                    tandemFloodFill(grid, ri1, ci1, row2, col2, !toggle);
+                }
             } else {
-                // just filled from seed 2, so let seed 1 proceed with old
-                // values
-                tandemFloodFill(grid, row1, col1, ri2, ci2, !toggle);
+                if (inBounds(grid, ri2, ci2) && grid[ri2][ci2] == 0) {
+                    tandemFloodFill(grid, row1, col1, ri2, ci2, !toggle);
+                }
             }
         }
     }
